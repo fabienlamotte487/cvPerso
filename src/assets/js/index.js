6bbc11f23c272b021cec512080299dde42a9c0da
fabienlamotte487 <goodplatine@gmail.com>
2025-04-15 16:00:34 +0200

    intégration certification opquast

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,8 @@
 import './../scss/style.scss';
 
 import './components/nav.js';
+document.addEventListener('DOMContentLoaded', () => {
+    const card = document.querySelector('.card-link');
+    card.classList.add('show');
+  });
+  
