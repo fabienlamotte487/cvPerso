8b39e83fadb3669748a6e3b04194d1cf1b2d6d1f
fabienlamotte487 <goodplatine@gmail.com>
2025-04-15 11:51:57 +0200

    Ajout de l'icone close menu burger en responsive + logique js pour faire fonctionner les deux boutons

--- src/assets/js/components/nav.js
+++ src/assets/js/components/nav.js
@@ -1,8 +1,16 @@
 const buttonBurger = document.getElementById("menuBurger");
+const buttonCloseBurger = document.getElementById("menuClose");
 const nav = document.querySelector("nav");
 
 document.addEventListener("DOMContentLoaded", function () {
     buttonBurger.addEventListener("click", function () {
-        nav.classList.toggle("display");
+        nav.classList.add("display");
+        buttonBurger.style.display = "none";
+        buttonCloseBurger.style.display = "block";
+    });
+    buttonCloseBurger.addEventListener("click", function () {
+        nav.classList.remove("display");
+        buttonBurger.style.display = "block";
+        buttonCloseBurger.style.display = "none";
     });
 });
