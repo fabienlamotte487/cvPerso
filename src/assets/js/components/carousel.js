48329417347a4c9c79570b24144bbdaad49425ec
fabienlamotte487 <goodplatine@gmail.com>
2025-04-23 14:30:24 +0200

    Ajout d'une fonction pour mettre en place le responsive correctement

--- src/assets/js/components/carousel.js
+++ src/assets/js/components/carousel.js
@@ -4,6 +4,7 @@ export class Carousel {
         this.list = this.carousel.querySelector('.carousel-list');
         this.buttons = this.carousel.querySelectorAll('.carousel-nav button');
         this.items = this.list.querySelectorAll('.carousel-item');
+        this.itemToShowDefault = itemToShow;
         this.itemToShow = itemToShow;
 
         this.currentIndex = 0;
@@ -26,6 +27,8 @@ export class Carousel {
         this.previousButton.addEventListener('click', this.decrement.bind(this));
         this.nextButton.addEventListener('click', this.increment.bind(this));
         this.disableOrEnableButtons();
+        this.updateResponsiveConfig();
+        window.addEventListener('resize', this.updateResponsiveConfig.bind(this));
     }
 
     checkElements() {
@@ -62,6 +65,25 @@ export class Carousel {
     updateCarousel() {
         const translatePercentage = -(this.currentIndex * 100);
         this.list.style.transform = `translateX(${translatePercentage}%)`;
-        
+    }
+
+    updateResponsiveConfig() {
+        const isMobile = window.innerWidth <= 768;
+        this.itemToShow = isMobile ? 1 : this.itemToShowDefault;
+    
+        this.maxIndex = Math.ceil(this.totalItems / this.itemToShow) - 1;
+    
+        if (this.currentIndex > this.maxIndex) {
+            this.currentIndex = this.maxIndex;
+        }
+    
+        const liElements = this.list.querySelectorAll('li');
+        liElements.forEach(li => {
+            li.style.flex = `0 0 ${100 / this.itemToShow}%`;
+        });
+    
+        // Update UI
+        this.updateCarousel();
+        this.disableOrEnableButtons();
     }
 }
